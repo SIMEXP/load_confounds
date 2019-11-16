27b1354711ff9955d70a23e4fe4ec7b57815e6a2
HanadS <Hanad@Hanads-MacBook-Pro.local>
2019-11-16 16:53:54 -0500

    replaced niftimasker function for nifti image function

--- fmri_clean.py
+++ fmri_clean.py
@@ -6,7 +6,8 @@ import pathlib as pal
 import pandas as pd
 import warnings
 from sklearn.decomposition import PCA
-from nilearn.input_data import NiftiLabelsMasker
+from nilearn.image import clean_img
+from nilearn.input_data import NiftiMasker
 from nilearn import datasets
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -92,21 +93,22 @@ def clean_nii(fmri_path,output,scrubbing):
     confounds_light = load_confounds(confounds_file,scrubbing)
 
     #Load Mask
-    parcellations = datasets.fetch_atlas_basc_multiscale_2015(version='sym')
-    networks_122 = parcellations['scale122']
-    masker = NiftiLabelsMasker(labels_img=networks_122, standardize=True, memory='nilearn_cache')
+    # parcellations = datasets.fetch_atlas_basc_multiscale_2015(version='sym')
+    # networks_122 = parcellations['scale122']
+    # masker = NiftiMasker()
 
     #clean nifti
-    img_data_regressed = masker.fit_transform(fmri_path, confounds=confounds_light.values)
+    img_data_clean = clean_img(fmri_path,confounds=confounds_light.values)
 
     #Save file
-    img_4d = nb.load(fmri_path)
-    nii_img = nb.Nifti1Image(img_data_regressed, img_4d.get_affine(), img_4d.get_header())
+    #img_4d = nb.load(fmri_path)
+    #print(img_data_clean.get_data())
+    #nii_img = nb.Nifti1Image(img_data_clean, img_data_clean.get_affine(), img_data_clean.get_header())
 
     #Save Nifti image
     fmri_path_out = str(output)+"/"+fmri_path.split("/")[-1].split(".nii.gz")[0]+"_clean.nii.gz"
     print("Saving file:",fmri_path_out.split("/")[-1], "\n")
-    nb.save(nii_img, fmri_path_out)
+    nb.save(img_data_clean, fmri_path_out)
 
     confounds_light.to_csv(str(output)+"/confounds_light.csv",index=False)
 
