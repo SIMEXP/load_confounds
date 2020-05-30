0fe758dfe67856ece14d2bd237fd729a8fb87380
pbellec <pierre.bellec@gmail.com>
2020-05-30 10:10:06 -0400

    Added a test for the different motion models.

--- tests/test_load_confounds.py
+++ tests/test_load_confounds.py
@@ -1,8 +1,10 @@
 import os
 from nilearn import datasets
 import load_confounds as lc
+import pandas as pd
 
 def _load_test_data():
+
     path_data = os.path.join(os.path.dirname(lc.__file__), 'data')
     return os.path.join(path_data, 'confounds.tsv')
 
@@ -16,3 +18,35 @@ def test_minimal():
     list_check = ['trans_x', 'rot_x', 'cosine00', 'csf', 'white_matter']
     for check in list_check:
         assert(check in conf.columns)
+
+
+def test_motion_model():
+    file_confounds = _load_test_data()
+
+    # Use a 6 params motion model
+    conf = lc.load_confounds(file_confounds, strategy=['minimal'], n_components=0, motion_model="6params")
+    assert('trans_x' in conf.columns)
+    assert('trans_x_derivative1' not in conf.columns)
+    assert('trans_x_power2' not in conf.columns)
+    assert('trans_x_derivative1_power2' not in conf.columns)
+
+    # Use a 6 params + derivatives motion model
+    conf = lc.load_confounds(file_confounds, strategy=['minimal'], n_components=0, motion_model="derivatives")
+    assert('trans_x' in conf.columns)
+    assert('trans_x_derivative1' in conf.columns)
+    assert('trans_x_power2' not in conf.columns)
+    assert('trans_x_derivative1_power2' not in conf.columns)
+
+    # Use a 6 params + square motion model
+    conf = lc.load_confounds(file_confounds, strategy=['minimal'], n_components=0, motion_model="square")
+    assert('trans_x' in conf.columns)
+    assert('trans_x_derivative1' not in conf.columns)
+    assert('trans_x_power2' in conf.columns)
+    assert('trans_x_derivative1_power2' not in conf.columns)
+
+    # Use a 6 params + square motion model
+    conf = lc.load_confounds(file_confounds, strategy=['minimal'], n_components=0, motion_model="full")
+    assert('trans_x' in conf.columns)
+    assert('trans_x_derivative1' in conf.columns)
+    assert('trans_x_power2' in conf.columns)
+    assert('trans_x_derivative1_power2' in conf.columns)
