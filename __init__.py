2a3e739b18d2f2013fd8dfba6be8bff81196d00d
pbellec <pierre.bellec@gmail.com>
2020-06-12 10:51:57 -0400

    Adding new denoising strategies to __init__

--- __init__.py
+++ __init__.py
@@ -1,6 +1,24 @@
 """
 loading fMRIprep confounds into python
 """
-from .load_confounds import Confounds, Params2, Params6, Params9, Params24, Params36
+from .load_confounds import (
+    Confounds,
+    Params2,
+    Params6,
+    Params9,
+    Params24,
+    Params36,
+    AnatCompCor,
+    TempCompCor,
+)
 
-__all__ = ["Confounds", "Params2", "Params6", "Params9", "Params24", "Params36"]
+__all__ = [
+    "Confounds",
+    "Params2",
+    "Params6",
+    "Params9",
+    "Params24",
+    "Params36",
+    "AnatCompCor",
+    "TempCompCor",
+]
