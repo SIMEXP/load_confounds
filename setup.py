706977e944787308c565d4c61bc149cd684b758a
pbellec <pierre.bellec@gmail.com>
2020-06-16 11:37:55 -0400

    Added richer description for pipy, and 0.6 version.

--- setup.py
+++ setup.py
@@ -1,11 +1,43 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
-   name='load_confounds',
-   version='0.5.dev',
-   description='load fMRIprep confounds in python',
-   author='SIMEXP',
-   packages=['.'],  #same as name
-   author_email='arnaud.bore@gmail.com',
-   install_requires=['numpy>=1.17.4', 'pandas>=0.25.3', 'scikit-learn>=0.21.3', 'scipy>=1.3.2', 'nilearn>=0.6.2'], #external packages as dependencies
+    name="load_confounds",
+    version="0.6",
+    description="load fMRIprep confounds in python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # Optional (see note above)
+    project_urls={  # Optional
+        "Bug Reports": "https://github.com/simexp/load_confounds/issues",
+        "Funding": "https://cneuromod.ca",
+        "Source": "https://github.com/simexp/load_confounds",
+    },
+    maintainer="Pierre Bellec",
+    maintainer_email="pierre.bellec@gmail.com",
+    packages=find_packages(),
+    author_email="arnaud.bore@gmail.com",
+    install_requires=[
+        "numpy>=1.17.4",
+        "pandas>=0.25.3",
+        "scikit-learn>=0.21.3",
+        "scipy>=1.3.2",
+        "nilearn>=0.6.2",
+    ],  # external packages as dependencies
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    python_requires=">=3.5",
 )
