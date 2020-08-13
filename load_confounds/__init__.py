4b4ac6e893fe26da8c9f6668a1d90fa859c87b24
pbellec <pierre.bellec@gmail.com>
2020-08-13 11:29:38 -0400

    Linting all code using black.

--- load_confounds/__init__.py
+++ load_confounds/__init__.py
@@ -1,8 +1,8 @@
 """
 loading fMRIprep confounds into python
 """
-from load_confounds.parser import (
-    Confounds,
+from load_confounds.parser import Confounds
+from load_confounds.strategies import (
     Params2,
     Params6,
     Params9,
