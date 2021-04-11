3cdd27de6450ab262f8b563aca45a30d89cf40b8
pbellec <pierre.bellec@gmail.com>
2021-04-11 00:07:52 -0400

    Adding missing strategies for import.

--- load_confounds/__init__.py
+++ load_confounds/__init__.py
@@ -4,10 +4,15 @@ from load_confounds.strategies import (
     Params2,
     Params6,
     Params9,
+    Params9Scrub,
     Params24,
     Params36,
+    Params36Scrub,
     AnatCompCor,
     TempCompCor,
+    ICAAROMA,
+    AROMAGSR,
+    AggrICAAROMA,
 )
 
 __all__ = [
@@ -15,8 +20,13 @@ __all__ = [
     "Params2",
     "Params6",
     "Params9",
+    "Params9Scrub",
     "Params24",
     "Params36",
+    "Params36Scrub",
     "AnatCompCor",
     "TempCompCor",
+    "ICAAROMA",
+    "AROMAGSR",
+    "AggrICAAROMA",
 ]
